5f556c4125c94d3f2d9eef7fa817da7c1462c3dc
Niroj Koirala <nirojk9@gmail.com>
2018-05-05 16:26:51 -0500

    some bugs

--- src/main/java/com/sagarmatha/serviceimpl/OrderServiceImpl.java
+++ src/main/java/com/sagarmatha/serviceimpl/OrderServiceImpl.java
@@ -1,11 +1,15 @@
 package com.sagarmatha.serviceimpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.sagarmatha.domain.Order;
 import com.sagarmatha.repository.OrderRepository;
 import com.sagarmatha.service.OrderService;
 
+@Service
+@Transactional
 public class OrderServiceImpl implements OrderService{
 	
 	@Autowired
